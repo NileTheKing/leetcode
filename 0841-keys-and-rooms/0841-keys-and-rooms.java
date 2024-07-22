a42459e22e5b404d5ce1331c194f9ba029f0e56a
NileTheKing <129058095+NileTheKing@users.noreply.github.com>
2024-07-22 15:22:01 +0900

    Time: 1 ms (77.79%), Space: 44.5 MB (11.76%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] visited = new boolean[rooms.size()];
+        visited[0] = true;  // 방 0은 이미 방문한 것으로 표시
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);  // 첫 번째 방을 스택에 추가
+        int count = 1;
+
+        while (!stack.isEmpty()) {
+            int room = stack.pop();  // 스택에서 방 번호를 꺼냄
+            for (int key : rooms.get(room)) {  // 현재 방에서 얻은 모든 키를 반복
+                if (!visited[key]) {  // 아직 방문하지 않은 방이면
+                    stack.push(key);  // 그 방을 스택에 추가
+                    visited[key] = true;  // 그 방을 방문한 것으로 표시
+                    count++;
+                }
+            }
+        }
+
+        return count == rooms.size();  // 모든 방을 방문했는지 확인
+    }
+}
