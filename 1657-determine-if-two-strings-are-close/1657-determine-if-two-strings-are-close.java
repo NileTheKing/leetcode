b2b19a6f16b90b9d44391b0b61b83e07550fbe23
NileTheKing <129058095+NileTheKing@users.noreply.github.com>
2024-08-26 18:04:00 +0900

    Time: 12 ms (55.43%), Space: 45.8 MB (17.79%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        if(word1.length() != word2.length())
+            return false;
+
+        int[] listA = new int[26];
+        int[] listB = new int[26];
+
+        for(int i = 0; i < listA.length; i++){ //초기화
+            listA[i] = 0;
+            listB[i] = 0;
+        }
+
+        
+        for(int i = 0; i < word1.length(); i++){ //빈도수
+            listA[word1.charAt(i) - 'a']++;
+            listB[word2.charAt(i) - 'a']++;
+        }
+
+
+        //비교
+
+        for(int i = 0; i < listA.length; i++){
+            if((listA[i] > 0 && listB[i] == 0) || (listA[i] == 0 && listB[i] > 0)){
+                return false;
+            }
+        }
+
+        //빈도수 비교
+        Arrays.sort(listA);
+        Arrays.sort(listB);
+
+        return Arrays.equals(listA, listB);
+
+    }
+}
