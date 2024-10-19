f9af7aef2893c8e4e0920649f65b5931e070e62d
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2024-10-19 19:53:24 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,20 +1,20 @@
 class Solution {
     public String removeStars(String s) {
-       Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
 
-       for(int i = 0; i < s.length(); i ++){
-        if(s.charAt(i) == '*'){
-            stack.pop();
-       }
-       else{
-        stack.push(s.charAt(i));
-       }
-    }
-
-        StringBuilder str = new StringBuilder();
-        for (char a : stack) {
-        str.append(a);      
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '*') {
+                stack.pop();
+            }else {
+                stack.push(s.charAt(i));
+            }
         }
-        return str.toString();
-}
+
+        for(char c : stack) {
+            sb.append(c)
+        }
+
+        return sb.toString();
+    }
 }
