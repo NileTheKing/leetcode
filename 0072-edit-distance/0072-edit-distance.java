3e40efe2c81993e8e052ebb8539897c854b8686b
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2024-12-04 17:02:06 +0900

    Time: 5 ms (67.35%), Space: 45.2 MB (25.22%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,28 +1,30 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int rows = word2.length() + 1;
-        int cols = word1.length() + 1;
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
 
-        int[][] dp = new int[rows][cols];
-
-        for(int i = 0; i < rows; i++){
-            dp[i][0] = i;
+        //세로 word1
+        for (int i = 0; i <= word1.length(); i++) { 
+            dp[i][0] = i; 
         }
 
-        for(int j = 0; j < cols; j++){
+        //가로 word2
+        for (int j = 0; j <= word2.length(); j++) {
             dp[0][j] = j;
         }
 
-        for(int i = 1; i < rows; i++){
-            for(int j = 1; j < cols; j++){
-                if(word1.charAt(j - 1) == word2.charAt(i - 1)) dp[i][j] = dp[i-1][j-1];
-                else{
-                    dp[i][j] = Math.min(dp[i-1][j-1] + 1
-                    , Math.min(dp[i-1][j] + 1, dp[i][j-1] + 1));
+
+        for(int i = 1; i <= word1.length(); i++) {
+            for(int j = 1; j <= word2.length(); j++) {
+                if(word1.charAt(i -1) == word2.charAt(j- 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }else {
+                    dp[i][j] = Math.min(Math.min(dp[i -1][j - 1] + 1, dp[i][j - 1] + 1),
+                                        dp[i-1][j] + 1);
                 }
             }
         }
 
-        return dp[rows - 1][cols - 1];
+        return dp[word1.length()][word2.length()];
+
     }
 }
